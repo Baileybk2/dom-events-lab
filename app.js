0279b4c960352530989e8b528753f7efac080943
BaileyBeck <baileybk2@gmail.com>
2024-06-03 17:39:09 -0400

    "dom events lab- calculator"

---
+++ app.js
@@ -0,0 +1,83 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const buttons = document.querySelectorAll(".button");
+console.log("Buttons:", buttons);
+
+/*-------------------------------- Variables --------------------------------*/
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = undefined;
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let clickedButton = event.target;
+    let clickButtonText = clickedButton.innerText;
+    let isNumber = clickedButton.classList.contains("number");
+    let isEquals = clickedButton.classList.contains("equals");
+    let isOperator = clickedButton.classList.contains("operator");
+
+    if (isNumber) {
+      if (operator === undefined) {
+        firstNumber += clickButtonText;
+        console.log("firstNumber:", firstNumber);
+      } else {
+        secondNumber += clickButtonText;
+        console.log("secondNumber:", secondNumber);
+      }
+    }
+
+    if (isOperator) {
+      if (clickButtonText === "C") {
+        firstNumber = "";
+        secondNumber = "";
+        operator = undefined;
+        console.log("claculator cleared!");
+      } else {
+        operator = clickButtonText;
+        console.log("operator:", operator);
+      }
+    }
+
+    if (isEquals) {
+      let num1 = Number(firstNumber);
+      let num2 = Number(secondNumber);
+
+      if (operator === "+") {
+        addition(num1, num2);
+        console.log(addition(num1, num2));
+      } else if (operator === "-") {
+        subtraction(num1, num2);
+        console.log(subtraction(num1, num2));
+      } else if (operator === "*") {
+        multiplication(num1, num2);
+        console.log(multiplication(num1, num2));
+      } else if (operator === "/") {
+        division(num1, num2);
+        console.log(division(num1, num2));
+      } else {
+        return "error";
+      }
+    }
+  });
+});
+
+/*-------------------------------- Functions --------------------------------*/
+const addition = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtraction = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiplication = (num1, num2) => {
+  return num1 * num2;
+};
+
+const division = (num1, num2) => {
+  return num1 / num2;
+};
